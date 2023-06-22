5d693d36795c107d02bda6671bda6cb60df13b53
Codesgenius <sannihabib001@gmail.com>
2023-06-22 01:59:56 +0100

    added render server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import { ToastContainer } from 'react-toastify';
-import {io} from 'socket.io-client'
-import Choice from './Chess/Choice';
-import Chess from './Chess/Chess'
-import Human from './Chess/Human';
-import Join from './Chess/Join';
-import Computer from './Chess/Computer';
-import './Chess/style.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { io } from "socket.io-client";
+import Choice from "./Chess/Choice";
+import Chess from "./Chess/Chess";
+import Human from "./Chess/Human";
+import Join from "./Chess/Join";
+import Computer from "./Chess/Computer";
+import "./Chess/style.css";
 
 // const socket = io(`http://${window.location.hostname}:8000`)
-const socket = io(`https://chessonweb.herokuapp.com`)
+// const socket = io(`https://chessonweb.herokuapp.com`)
+const socket = io(`https://chessbackend-fjpg.onrender.com`);
 
 function App() {
   return (
     <BrowserRouter>
-    <ToastContainer />
+      <ToastContainer />
       <Routes>
-        <Route path='/' element={<Choice socket={socket} />} />
-        <Route path='/searchplayer' element={<Join socket={socket} />} />
-        <Route path='/local/humanvshuman' element={<Chess socket={socket} />} />
-        <Route path='/local/humanvscomputer' element={<Computer socket={socket} />} />
-        <Route path='/online/humanvshuman' element={<Human socket={socket} />} />
+        <Route path="/" element={<Choice socket={socket} />} />
+        <Route path="/searchplayer" element={<Join socket={socket} />} />
+        <Route path="/local/humanvshuman" element={<Chess socket={socket} />} />
+        <Route
+          path="/local/humanvscomputer"
+          element={<Computer socket={socket} />}
+        />
+        <Route
+          path="/online/humanvshuman"
+          element={<Human socket={socket} />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/Chess/utils/socket.js
+++ src/Chess/utils/socket.js
@@ -1,2 +1,3 @@
 import {io} from 'socket.io-client'
+// export const socket = io(`http://${window.location.hostname}:8000`)
 export const socket = io(`http://${window.location.hostname}:8000`)
